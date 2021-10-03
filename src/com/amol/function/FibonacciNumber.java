aa7a61f82d1ddec2eff20fbf4650336fac46bedd
Amol Dogra <amol88658@gmail.com>
2021-10-03 17:40:06 +0530

    Using Recursion Method

---
+++ src/com/amol/function/FibonacciNumber.java
@@ -0,0 +1,19 @@
+package com.amol.function;
+
+import java.util.Scanner;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+        System.out.println(fibo(n));
+
+    }
+    static int fibo(int n){
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
