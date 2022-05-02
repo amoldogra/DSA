73c70fdb4238cd8b01795dbfcbf977e5c6242a35
Amol Dogra <amol88658@gmail.com>
2022-05-02 23:44:42 +0530

    Update math

---
+++ src/com/amol/maths/Calculator.java
@@ -0,0 +1,45 @@
+package com.amol.maths;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ans =0;
+
+         while(true){
+
+             System.out.println("Enter the Opertaor : ");
+             char ch = in.next().trim().charAt(0);
+            if(ch == '+' || ch == '-' || ch == '*' || ch == '%' || ch == '/'){
+
+                System.out.println("Enter Two Numbers : ");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+
+                if(ch == '+'){
+                    ans = num1 + num2;
+                }
+                if(ch == '-'){
+                    ans = num1 - num2;
+                }
+                if(ch == '*'){
+                    ans = num1 * num2;
+                }
+                if(ch == '/'){
+                    ans = num1 / num2;
+                }
+                if(ch == '%'){
+                    ans = num1 % num2;
+                }
+                else if(ch == 'x' || ch == 'X'){
+                   break;
+                }
+                else {
+                    System.out.println("INVALID INPUT");
+                }
+            }
+         }
+
+    }
+}
---
+++ src/com/amol/maths/Reverse.java
@@ -0,0 +1,14 @@
+package com.amol.maths;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int num = 24264;
+         int ans =0;
+         while(num> 0){
+             int rem =  num % 10;
+             num /= 10;
+             ans = ans * 10 +rem;
+         }
+        System.out.println(ans);
+    }
+}
